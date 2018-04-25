40b227693f6a7340b7f72f86a77d71a720458564
Aakriti <aakriti1711@gmail.com>
2018-04-25 13:25:51 -0700

    Added transitio effect to keys

--- Javascript Drum Kit/script.js
+++ Javascript Drum Kit/script.js
@@ -1,9 +1,22 @@
-window.addEventListener('keydown',function(e){
-const audio= document.querySelector(`audio[data-key="${e.keyCode}"`);
-if(!audio) return; // stop the function from running all together
+window.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+    if (!audio) return; // stop the function from running all together
 
-audio.currentTime = 0; // REWIND TO START
-audio.play();
-//console.log(e);
-
+    audio.currentTime = 0; // REWIND TO START
+    audio.play();
+    key.classList.add('playing');
 });
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if its not a transform
+
+    this.classList.remove('playing');
+
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => function (key) {
+    console.log(key);
+})
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
